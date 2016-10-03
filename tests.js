debfec9256b3d347705c7a10292e2c78a686da93
Jaicomp <jaicompdeveloper@gmail.com>
2016-10-03 11:25:16 +0200

    Added function testCase

--- tests.js
+++ tests.js
@@ -18,3 +18,33 @@ function output(text, color) {
 	p.style.color = color;
 	document.body.appendChild(p);
 }
+
+//For checking a test, 
+function testCase(name, tests) {
+	assert.count = 0;
+	var successful = 0;
+	var testCount = 0;
+	var successfulColor = "#0c0"	
+	var failureColor = "#c00"
+
+	for (var test in tests) {
+		if (!/^test/.test(test)) {
+			continue;
+		}
+		
+		testCount++;
+
+		try {
+			tests[test]();
+			output(test, successfulColor);
+			successful++;
+		} catch (e) {
+			output(test + " failed: " + e.message, failureColor);
+		}
+
+	}
+	var color = successful == testCount ? successfulColor : failureColor;
+
+	output("<strong>" + testCount + " tests, " + (testCount - successful) + "failures</strong", color);
+	
+}
